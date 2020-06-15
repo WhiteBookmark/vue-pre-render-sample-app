713afda83ef85ee1e1b09214826824d99b38802f
Rein <37548818+WhiteBookmark@users.noreply.github.com>
2020-06-15 12:25:51 +0500

    style: lint entire codebase for required semicolon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
 @Component
 export default class HelloWorld extends Vue{
