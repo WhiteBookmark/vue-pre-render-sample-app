2cecaa7d8b0e406f630eedfc229dc8fd1cf521d3
Rein <37548818+WhiteBookmark@users.noreply.github.com>
2020-06-16 10:11:13 +0500

    fix: split dev and production webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,34 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+if (process.env.NODE_ENV === 'production') {
+	const path = require('path');
+	const PrerenderSPAPlugin = require('prerender-spa-plugin');
+	const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
-module.exports = {
-	transpileDependencies: ['vuetify'],
-	configureWebpack: {
-		plugins: [
-			// Only prerender for production build
-			process.env.NODE_ENV === 'production' ? new PrerenderSPAPlugin({
-				// Required - The path to the webpack-outputted app to prerender.
-				staticDir: path.join(__dirname, 'dist'),
-				// Required - Routes to render.
-				routes: ['/'],
+	module.exports = {
+		transpileDependencies: ['vuetify'],
+		configureWebpack: {
+			plugins: [
+				// Only prerender for production build
+				new PrerenderSPAPlugin({
+					// Required - The path to the webpack-outputted app to prerender.
+					staticDir: path.join(__dirname, 'dist'),
+					// Required - Routes to render.
+					routes: ['/'],
 
-				renderer: new Renderer({
-					// Optional - Wait to render until the specified event is dispatched on the document.
-					// eg, with `document.dispatchEvent(new Event('axios-operation-completed'))`
-					renderAfterDocumentEvent: 'axios-operation-completed',
-					// Wait further 5 seconds otherwise data won't be rendered
-					renderAfterTime: 10000,
-					headless: false,
+					renderer: new Renderer({
+						// Optional - Wait to render until the specified event is dispatched on the document.
+						// eg, with `document.dispatchEvent(new Event('axios-operation-completed'))`
+						renderAfterDocumentEvent: 'axios-operation-completed',
+						// Wait further 5 seconds otherwise data won't be rendered
+						renderAfterTime: 10000,
+						headless: false,
+					}),
 				}),
-			}) : '',
-		],
-	},
-};
+			],
+		},
+	};
+} else {
+	module.exports = {
+		transpileDependencies: ['vuetify'],
+	};
+}
