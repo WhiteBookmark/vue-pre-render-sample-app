a8fd166ea0de960cb190f7bd6b83c74680fd7901
Rein <37548818+WhiteBookmark@users.noreply.github.com>
2020-06-16 10:22:52 +0500

    chore: add eslint parser and change plugin extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,18 @@ module.exports = {
 	env: {
 		node: true,
 	},
-	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended'],
+	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript'],
 	parserOptions: {
 		ecmaVersion: 2020,
+		parser: '@typescript-eslint/parser',
 	},
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		quotes: ['error', 'single'],
-		indent: ['error', 'tab'],
+		'quotes': ['error', 'single'],
+		'indent': ['error', 'tab'],
 		'max-len': ['error', 200],
 		'semi': 'error',
-		'@typescript-eslint/no-explicit-any': 'off'
+		'@typescript-eslint/no-explicit-any': 'off',
 	},
 };
